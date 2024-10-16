d7b7cc02533cdbaac824fbc7b0e98f0f00bf55df
achmelo <37397715+achmelo@users.noreply.github.com>
2024-10-16 17:19:18 +0200

    rename branch and repo

--- renovate-config.js
+++ renovate-config.js
@@ -1,38 +1,17 @@
 module.exports = {
     globalExtends: ["config:recommended"], // using this instead of "extends" solves the problem with order of the configuration
-    repositories: ['zowe/api-layer'],
-    baseBranches: ["v2.x.x","v3.x.x"],
+    repositories: ['achmelo/api-layer'],
+    baseBranches: ["master"],
     dependencyDashboard: true,
     packageRules: [
         {
-            //for v2.x.x branch ignore grouping from extends preset, find all packages which are patches,
-            // slug them and make PR with name "all patch dependencies"
-            "matchBaseBranches": ["v2.x.x"],
-            "groupName": "all patch dependencies",
-            "groupSlug": "all-patch",
-            "matchPackageNames": ["*"],
-            "matchUpdateTypes": ["patch"],
-        },
-        {
-            //for v2.x.x make dashboard approval to all major and minor dependencies updates
-            "matchBaseBranches": ["v2.x.x"],
-            "matchUpdateTypes": ["major", "minor"],
-            "dependencyDashboardApproval": true,
-        },
-        {
             //for v3.x.x branch find all packages which are minor and patches,
             // slug them and make PR with name "all non-major dependencies"
-            "matchBaseBranches": ["v3.x.x"],
+            "matchBaseBranches": ["master"],
             "groupName": "all non-major dependencies",
             "groupSlug": "all-minor-patch",
             "matchPackageNames": ["*"],
             "matchUpdateTypes": ["minor", "patch"]
-        },
-        {
-            //for v3.x.x make dashboard approval to all major dependencies updates
-            "matchBaseBranches": ["v3.x.x"],
-            "matchUpdateTypes": ["major"],
-            "dependencyDashboardApproval": true,
         }
     ],
     printConfig: true,
